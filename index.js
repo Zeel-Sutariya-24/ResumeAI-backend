000b7cd34c70dd036c1dbe9b09dab1e6e6a805be
Zeel-sutariya-24 <zeelsutariya24@gmail.comm>
2025-12-23 12:16:38 -0700

    fix: listen on render PORT

--- index.js
+++ index.js
@@ -36,10 +36,13 @@ mongoose
   .connect(process.env.MONGO_URI)
   .then(() => {
     console.log("MongoDB connected");
+    
+    const PORT = process.env.PORT || 5000;
 
-    app.listen(5000, () => {
-      console.log("Backend running on port 5000");
+    app.listen(PORT, () => {
+      console.log(`Server running on port ${PORT}`);
     });
+
   })
   .catch((err) => {
     console.error("MongoDB connection error:", err.message);
